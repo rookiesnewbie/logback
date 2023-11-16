b7946fabeabd1174f9a14046513cb25fe6c68c03
rookiesnewbie <2049448867@qq.com>
2023-11-16 20:59:36 +0800

    级别过滤器

--- logback_filter/src/main/java/com/logback/App.java
+++ logback_filter/src/main/java/com/logback/App.java
@@ -1,13 +1,19 @@
 package com.logback;
 
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    static ch.qos.logback.classic.Logger logger =
+            (ch.qos.logback.classic.Logger) LoggerFactory.getLogger("com.aa");
+    public static void main(String[] args) {
+
+        logger.info( "Hello World!" );
+
+
     }
 }
