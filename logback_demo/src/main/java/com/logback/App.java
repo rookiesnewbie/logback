24cbc5b22fc081f6600562bc8891245b44d2b812
rookiesnewbie <2049448867@qq.com>
2023-11-16 13:48:05 +0800

    创建记录器

--- logback_demo/src/main/java/com/logback/App.java
+++ logback_demo/src/main/java/com/logback/App.java
@@ -1,13 +1,15 @@
 package com.logback;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 /**
  * Hello world!
- *
+ *   日志级别：TRACE < DEBUG < INFO < WARN < ERROR
  */
 public class App {
-    Logger
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    //创建日志记录器
+    static Logger logger = LoggerFactory.getLogger(App.class); //"com.logback.App"
+    public static void main( String[] args ) {
+        logger.error("Hello World!");
     }
 }
